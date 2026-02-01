54552756b5f50501b7318fd1c5275f95dd7ec12f
matthewaasen <112990586+matthewaasen@users.noreply.github.com>
2026-02-01 16:48:21 -0600

    added fruit drop mechanic

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerBehavior : MonoBehaviour
 {
     public float speed; //public means you can edit it in Unity Inspector
+    public GameObject fruit;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +16,20 @@ public class PlayerBehavior : MonoBehaviour
     
     void Update()
     {
+        if (fruit != null)
+        {
+            Vector3 fruitOffset = new Vector3(0.0f, -1.0f, 0.0f);
+            fruit.transform.position = gameObject.transform.position + fruitOffset; 
+            fruit.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+            fruit.GetComponent<Collider2D>().enabled = false;
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            fruit.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+            fruit.GetComponent<Collider2D>().enabled = true;
+            fruit = null;
+        }
         Keyboard kb = Keyboard.current;
         if (kb.leftArrowKey.isPressed || kb.aKey.isPressed)
         {
