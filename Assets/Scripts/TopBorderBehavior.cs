f976ac4104ef88797b8088dbc4f3759fb1ba9b82
matthewaasen <112990586+matthewaasen@users.noreply.github.com>
2026-02-12 15:12:58 -0600

    added game over screen

--- Assets/Scripts/TopBorderBehavior.cs
+++ Assets/Scripts/TopBorderBehavior.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class TopBorderBehavior : MonoBehaviour
 {
     public float timeout;
     private float timeStart;
+    public TMP_Text gameOverText;
+    public GameObject gameOverPanel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        gameOverText.gameObject.SetActive(false);
+        gameOverPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,7 +29,13 @@ public class TopBorderBehavior : MonoBehaviour
     {
         if (Time.time - timeStart > timeout)
         {
-            print("Game Over");
+            gameOverText.gameObject.SetActive(true);
+            gameOverPanel.SetActive(true);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        timeStart = 0.0f;
     }
 }
